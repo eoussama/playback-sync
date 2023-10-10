b989463641cef357e4e54b0e005231804c8467d1
EOussama <oussama_gtacj@live.fr>
2023-10-10 11:58:21 +0100

    📼 List Sources, resolves #16

--- src/components/View.vue
+++ src/components/View.vue
@@ -11,7 +11,45 @@ export default defineComponent({
 </script>
 
 <template>
-  View {{ sources }}
+  <div class="sources">
+    <div
+      class="source"
+      v-for="source in sources"
+    >
+      <div class="source__head">
+        <div class="source_title">
+          <a
+            target="_blank"
+            :href="source.url"
+          >{{ source.title }}</a>
+        </div>
+      </div>
+      <div class="source__body">
+        <video
+          controls
+          class="source__player"
+        >
+          <source
+            :src="source.url"
+            type="video/mp4"
+          >
+        </video>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.sources {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  .source {
+    max-width: 100%;
+
+    &__player {
+      width: 100%;
+    }
+  }
+}
+</style>
