75c60338faabea09757168c8194e9c45b83ad83f
EOussama <oussama_gtacj@live.fr>
2023-10-11 23:18:45 +0100

    ⏲️ Mute, fixes #27

--- src/utils/helpers/time.helper.ts
+++ src/utils/helpers/time.helper.ts
@@ -15,10 +15,11 @@ export class TimeHelper {
    * @param seconds The number of seconds to format
    */
   static secondsToTime(seconds: TUnsafe<number>): string {
-    const value = seconds ?? 0;
+    const value = Math.max(seconds ?? 0, 0);
+    const safeValue = isNaN(value) ? 0 : value;
 
-    const secs = Math.floor(value % 60);
-    const mins = Math.floor(value / 60);
+    const secs = Math.floor(safeValue % 60);
+    const mins = Math.floor(safeValue / 60);
 
     const secsStr = secs.toString().padStart(2, '0');
     const minsStr = mins.toString().padStart(2, '0');
