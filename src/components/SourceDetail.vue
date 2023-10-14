b846aba19fc09d118623d137cef54ca2e7d417e2
EOussama <oussama_gtacj@live.fr>
2023-10-14 13:06:59 +0100

    ➕ Source Addition Form, #32

--- src/components/SourceDetail.vue
+++ src/components/SourceDetail.vue
@@ -7,7 +7,43 @@ export default defineComponent({
 </script>
 
 <template>
-  Source Detail
+  <div class="source">
+    <div class="source__form">
+      <div class="input">
+        <label class="input__wrapper">
+          <div class="input__label">Source Title</div>
+          <div class="input__input">
+            <input
+              type="text"
+              placeholder="Enter a title for the source"
+            >
+          </div>
+        </label>
+      </div>
+      
+      <div class="input">
+        <label class="input__wrapper">
+          <div class="input__label">Source URL</div>
+          <div class="input__input">
+            <input
+              type="url"
+              placeholder="Enter the URL of the source"
+            >
+          </div>
+        </label>
+      </div>
+    </div>
+
+    <div class="source__control">
+      <button class="button">
+        Reset
+      </button>
+    
+      <button class="button">
+        Add
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
