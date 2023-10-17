f058dcdcb0ef0e275e3f19164ecd942c2486ebb0
EOussama <oussama_gtacj@live.fr>
2023-10-17 14:29:22 +0100

    🪖 Tilling Component, #65

--- src/components/controls/Tilling.vue
+++ src/components/controls/Tilling.vue
@@ -1,14 +1,26 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { TillingValue } from '@/utils/enums/tillingValue.enum';
 
 export default defineComponent({
 
+  data: () => ({
+    value: TillingValue.Fill,
+    options: [
+      { label: 'Fill', value: TillingValue.Fill },
+      { label: 'Split', value: TillingValue.Split },
+      { label: 'Custom', value: TillingValue.Custom }
+    ]
+  })
 });
 </script>
 
 <template>
   <div class="tilling">
-    Tilling
+    <Select
+      v-model="value"
+      :options="options"
+    />
   </div>
 </template>
 
---
+++ src/utils/enums/tillingValue.enum.ts
@@ -0,0 +1,5 @@
+export enum TillingValue {
+  Fill = 0,
+  Split = 1,
+  Custom = 3
+}
