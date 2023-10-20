71279dc19a1bc66aba8e2afa7be9064be124012b
EOussama <oussama_gtacj@live.fr>
2023-10-20 13:35:43 +0100

    🖌️ Foot Styling, #74

--- src/components/layout/FootSection.vue
+++ src/components/layout/FootSection.vue
@@ -8,11 +8,68 @@ export default defineComponent({});
   <div class="foot">
     <a
       target="_blank"
+      class="foot__content"
       href="https://github.com/EOussama/playback-sync"
-    >Playback Sync</a>
+    >
+      <span class="foot__github">
+        <font-awesome-icon icon="fa-brands fa-github" />
+      </span>
+
+      <span class="foot__text">
+        Playback Sync
+      </span>
+    </a>
   </div>
 </template>
 
 <style scoped lang="scss">
-.foot {}
+.foot {
+  padding: 2px 10px;
+
+  box-sizing: border-box;
+  background-color: hsl(var(--color-secondary-hsl), 96%);
+
+  &__content {
+    text-align: center;
+    text-decoration: none;
+
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+
+      .foot__text,
+      .foot__github {
+        color: hsl(var(--color-secondary-hsl), 50%);
+        color: hsl(var(--color-secondary-hsl), 50%);
+      }
+    }
+  }
+
+  &__text,
+  &__github {
+    color: hsl(var(--color-secondary-hsl), 65%);
+
+    transition-duration: 0.2s;
+    transition-property: color;
+  }
+
+  &__github {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 14px;
+    margin-right: 5px;
+  }
+
+  &__text {
+    font-size: 12px;
+    font-family: var(--font-family);
+  }
+}
 </style>
