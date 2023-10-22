1641f3a682d4c272cdc31895af5ab9147b0859ff
EOussama <oussama_gtacj@live.fr>
2023-10-22 13:49:37 +0100

    🔽 Select Styling, #74

--- src/components/controls/SelectComp.vue
+++ src/components/controls/SelectComp.vue
@@ -6,12 +6,12 @@ export default defineComponent({
   emits: ['update:modelValue', 'changed'],
 
   props: {
+    modelValue: String,
+    placeholder: String,
     options: {
       default: () => [],
       type: Array as PropType<Array<TOption>>
-    },
-    modelValue: String,
-    placeholder: String
+    }
   },
 
   methods: {
@@ -34,50 +34,78 @@ export default defineComponent({
 </script>
 
 <template>
-  <select
-    class="select"
-    :value="modelValue"
-    @change="onChanged"
-  >
-    <option
-      value=""
-      v-if="placeholder"
-      class="select__option select__option--default"
+  <div class="select-wrapper">
+    <select
+      class="select"
+      :value="modelValue"
+      @change="onChanged"
     >
-      {{ placeholder }}
-    </option>
+      <option
+        value=""
+        v-if="placeholder"
+        class="select__option select__option--default"
+      >
+        {{ placeholder }}
+      </option>
 
-    <option
-      :key="index"
-      :value="option.value"
-      class="select__option"
-      v-for="(option, index) in options"
-    >
-      {{ option.label }}
-    </option>
-  </select>
+      <option
+        :key="index"
+        :value="option.value"
+        class="select__option"
+        v-for="(option, index) in options"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+
+    <div class="select-icon">
+      <font-awesome-icon icon="chevron-down" />
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @use '@/style/mixins/selectable';
 
-.select {
-  font-size: 14px;
-  color: var(--color-primary);
-  font-family: var(--font-primary);
 
+.select-wrapper {
   width: 100%;
-  height: 38px;
-  padding: 0 10px;
+  position: relative;
 
-  border-radius: 5px;
-  box-sizing: border-box;
-  border: 1px solid transparent;
-  background-color: hsl(var(--color-secondary-hsl), 86%);
+  .select {
+    display: block;
+    
+    font-size: 14px;
+    text-align: center;
+    color: var(--color-primary);
+    font-family: var(--font-primary);
+    
+    width: 100%;
+    height: 38px;
+    padding: 0 30px 0 10px;
 
-  transition-duration: 0.2s;
-  transition-property: background-color;
+    appearance: none;
 
-  @extend %selectable;
+    border-radius: 5px;
+    box-sizing: border-box;
+    border: 1px solid transparent;
+    background-color: hsl(var(--color-secondary-hsl), 86%);
+
+    transition-duration: 0.2s;
+    transition-property: background-color;
+
+    @extend %selectable;
+  }
+
+  .select-icon {
+    pointer-events: none;
+
+    position: absolute;
+    top: 50%;
+    right: 11px;
+    transform: translateY(-50%);
+
+    font-size: 12px;
+  }
 }
 </style>
--- src/utils/helpers/fontawesome.helper.ts
+++ src/utils/helpers/fontawesome.helper.ts
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import {
-  faThumbTack, faTrash,
+  faThumbTack, faTrash, faChevronDown,
   faPlay, faPause, faRepeat, faBackward, faForward,
   faVolumeOff, faVolumeLow, faVolumeHigh, faVolumeXmark,
   faPlus, faXmark, faCheck, faPen, faTriangleExclamation
@@ -15,7 +15,7 @@ import {
  */
 export function loadIcons(): void {
   library.add(
-    faThumbTack, faTrash, faGithub,
+    faThumbTack, faTrash, faGithub, faChevronDown,
     faPlay, faPause, faRepeat, faBackward, faForward,
     faVolumeOff, faVolumeLow, faVolumeHigh, faVolumeXmark,
     faPlus, faXmark, faCheck, faPen, faTriangleExclamation
