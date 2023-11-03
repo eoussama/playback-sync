ec887425849624d9520717ead7306e6d69e160a5
EOussama <oussama_gtacj@live.fr>
2023-11-03 13:18:16 +0100

    ⌨️ Fullscreen Shortcut, #89

---
+++ src/utils/enums/key.enum.ts
@@ -0,0 +1,13 @@
+export enum Key {
+  Close = 'Escape',
+  Fullscreen = 'F11',
+  ToggleFullscreen = 'KeyF',
+  TogglePlay = 'Space',
+  ToggleMute = 'Semicolon',
+  Backward = 'ArrowLeft',
+  Forward = 'ArrowRight',
+  VolumeUp = 'ArrowUp',
+  VolumeDown = 'ArrowDown',
+  SpeedUp = 'NumpadAdd',
+  SpeedDown = 'NumpadSubtract'
+}
--- src/utils/helpers/shortcut.helper.ts
+++ src/utils/helpers/shortcut.helper.ts
@@ -1,7 +1,58 @@
+import { Key } from '../enums/key.enum';
+
+import { useAppStore } from '@/state/stores/app.store';
+
+
+
 /**
  * @description
  * Initializes the shortcut manager
  */
 export function initShortcuts(): void {
+  const appStore = useAppStore();
 
+  window.onkeydown = ({ code }) => {
+    if (code === Key.Fullscreen) {
+      return false;
+    }
+  }
+
+  window.onkeyup = ({ code }) => {
+    const detectShortcut = document.activeElement?.nodeName.toLocaleLowerCase() !== 'input';
+
+    if (detectShortcut) {
+      switch (code) {
+        case Key.ToggleFullscreen: {
+          appStore.toggleFullscreen();
+          break;
+        }
+      }
+    }
+  }
 }
+
+// /**
+//  * @description
+//  * Helps with shortcuts
+//  */
+// export class ShortcutHelper {
+
+//   /**
+//    * @description
+//    * Listens for key events
+//    *
+//    * @param key The key to listen to
+//    */
+//   static async on(key: string): Promise<void> {
+//     return new Promise(resolve => {
+//       const listener = (e: KeyboardEvent) => {
+//         if (e.code === key) {
+//           resolve();
+//           window.removeEventListener('keyup', listener);
+//         }
+//       }
+
+//       window.addEventListener('keyup', listener);
+//     });
+//   }
+// }
