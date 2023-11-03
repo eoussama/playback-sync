13cbb30c72a6d3bddcd6ae66e2755826215e51b3
EOussama <oussama_gtacj@live.fr>
2023-11-03 14:02:43 +0100

    🐛 Key Detection, #126

--- src/utils/helpers/shortcut.helper.ts
+++ src/utils/helpers/shortcut.helper.ts
@@ -27,72 +27,68 @@ export function initShortcuts(): void {
   window.onkeyup = ({ code }) => {
     const detectShortcut = document.activeElement?.nodeName.toLocaleLowerCase() !== 'input';
 
-    switch (code) {
-      case Key.ToggleFullscreen: {
-        if (detectShortcut) {
+    if (detectShortcut) {
+      switch (code) {
+        case Key.ToggleFullscreen: {
           appStore.toggleFullscreen();
+          break;
         }
 
-        break;
+        case Key.Add: {
+          const addButton = document.getElementById('button-add-modal');
+          addButton?.click();
+
+          break;
+        }
+
+        case Key.ToggleMute: {
+          sourceStore.setMuted(!sourceStore.muted);
+          break;
+        }
+
+        case Key.TogglePlay: {
+          sourceStore.setPlaying(!sourceStore.playing);
+          break;
+        }
+
+        case Key.Forward: {
+          sourceStore.onSeek(appStore.seekStep);
+          break;
+        }
+
+        case Key.Backward: {
+          sourceStore.onSeek(-appStore.seekStep);
+          break;
+        }
+
+        case Key.VolumeUp: {
+          sourceStore.setVolume(sourceStore.volume + appStore.volumeStep);
+          break;
+        }
+
+        case Key.VolumeDown: {
+          sourceStore.setVolume(sourceStore.volume - appStore.volumeStep);
+          break;
+        }
+
+        case Key.SpeedUp: {
+          const speedIndex = speed.indexOf(sourceStore.speed);
+          const index = MathHelper.clamp(speedIndex + 1, 0, speed.length - 1);
+
+          sourceStore.setSpeed(speed[index]);
+          break;
+        }
+
+        case Key.SpeedDown: {
+          const speedIndex = speed.indexOf(sourceStore.speed);
+          const index = MathHelper.clamp(speedIndex - 1, 0, speed.length - 1);
+
+          sourceStore.setSpeed(speed[index]);
+          break;
+        }
       }
-
-      case Key.Close: {
-        modalStore.closeModal();
-        break;
-      }
-
-      case Key.Add: {
-        const addButton = document.getElementById('button-add-modal');
-        addButton?.click();
-
-        break;
-      }
-
-      case Key.ToggleMute: {
-        sourceStore.setMuted(!sourceStore.muted);
-        break;
-      }
-
-      case Key.TogglePlay: {
-        sourceStore.setPlaying(!sourceStore.playing);
-        break;
-      }
-
-      case Key.Forward: {
-        sourceStore.onSeek(appStore.seekStep);
-        break;
-      }
-
-      case Key.Backward: {
-        sourceStore.onSeek(-appStore.seekStep);
-        break;
-      }
-
-      case Key.VolumeUp: {
-        sourceStore.setVolume(sourceStore.volume + appStore.volumeStep);
-        break;
-      }
-
-      case Key.VolumeDown: {
-        sourceStore.setVolume(sourceStore.volume - appStore.volumeStep);
-        break;
-      }
-
-      case Key.SpeedUp: {
-        const speedIndex = speed.indexOf(sourceStore.speed);
-        const index = MathHelper.clamp(speedIndex + 1, 0, speed.length - 1);
-
-        sourceStore.setSpeed(speed[index]);
-        break;
-      }
-
-      case Key.SpeedDown: {
-        const speedIndex = speed.indexOf(sourceStore.speed);
-        const index = MathHelper.clamp(speedIndex - 1, 0, speed.length - 1);
-
-        sourceStore.setSpeed(speed[index]);
-        break;
-      }
+    } else if (code === Key.Close) {
+      modalStore.closeModal();
     }
   }
 }
