9e0f3107e4a3a9213387f0a288e053dbb8cc38f4
EOussama <oussama_gtacj@live.fr>
2023-11-07 13:38:21 +0100

    🐶 Volume More, #143

--- src/components/layout/ControlsComp.vue
+++ src/components/layout/ControlsComp.vue
@@ -5,6 +5,8 @@ import { mapState, mapActions } from 'pinia';
 import { useAppStore } from '@/state/stores/app.store';
 import { useSourcesStore } from '@/state/stores/sources.store';
 
+import { getVolumeIcon } from '@/utils/helpers/fontawesome.helper';
+
 export default defineComponent({
 
   methods: {
@@ -117,10 +119,18 @@ export default defineComponent({
 
     /**
      * @description
+     * The icon to show on the button
+     */
+    icon(): string {
+      return getVolumeIcon(this.volume, this.muted);
+    },
+
+    /**
+     * @description
      * The universal duration,
      * generally the duration of the longest source
      */
-    duration() {
+    duration(): number {
       return Math.max(...this.sources.map(e => e.metadata.duration));
     },
 
@@ -129,7 +139,7 @@ export default defineComponent({
      * Returns the current time of the longest loaded source
      * to use a a reference for universal time
      */
-    timelineValue() {
+    timelineValue(): number {
       return this.longestSource?.metadata?.currentTime ?? 0;
     },
 
@@ -137,7 +147,7 @@ export default defineComponent({
      * @description
      * Whether or not the sources are finished playing
      */
-    ended() {
+    ended(): boolean {
       return this.timelineValue === this.duration
     },
 
@@ -146,7 +156,7 @@ export default defineComponent({
      * If the controls are disabled, mainly due
      * to the absense of any loaded sources.
      */
-    disabled() {
+    disabled(): boolean {
       return this.sources.length === 0;
     }
   }
@@ -217,6 +227,19 @@ export default defineComponent({
           />
         </div>
 
+        <div class="controls__volume controls__volume--more">
+          <MoreComp
+            :icon="icon"
+            type="primary"
+          >
+            <VolumeComp
+              :muted="muted"
+              :value="volume"
+              @volumeUpdated="onVolume"
+              @muteToggled="onMuteToggled"
+            />
+          </MoreComp>
+        </div>
       </div>
     </div>
   </div>
@@ -270,6 +293,10 @@ export default defineComponent({
         margin: 0 10px;
       }
     }
+
+    #{$root}__volume--more {
+      display: none;
+    }
   }
 
   &-wrapper {
@@ -319,21 +346,14 @@ export default defineComponent({
       #{$root}__rewind {
         transform: translate(-50%, calc(-50% - 100px));
       }
-    }
-  }
-
-  @include utils.responsive('phone-sm') {
-    &__bottom {
-      flex-direction: column-reverse;
-
-      #{$root}__rewind {
-        transform: translate(-50%, calc(-50% - 125px));
-      }
 
       #{$root}__volume {
-        margin-bottom: 15px;
+        display: none;
+
+        &--more {
+          display: block;
+        }
       }
     }
   }
-}
-</style>
+}</style>
